0600e36f3518b3dec9c9f917e9c93fdcf3e510ea
safri1905 <47934961+safri1905@users.noreply.github.com>
2022-04-05 01:06:00 +0700

    Update downloader.py

--- program/downloader.py
+++ program/downloader.py
@@ -70,7 +70,8 @@ async def song_downloader(_, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"• uploader @{bn}"
+        rep = f"{title}\n\n"
+              f"• uploader @{bn}"
         host = str(info_dict["uploader"])
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
